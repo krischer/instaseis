fa821327d438ea61c7264ec716022d4a5c85b608
martinvandriel <martin@vandriel.de>
2014-09-03 11:53:30 +0200

    adding a docstring

--- axisem_db/srf.py
+++ axisem_db/srf.py
@@ -17,6 +17,14 @@ from .source import Source
 DEFAULT_MU = 32e9
 
 def read_srf(filename, normalize=False):
+    """
+    Read a 'standard rupture format' (.srf) file and return a list of axisem_db.Source
+    objects.
+
+    Keyword arguments:
+    filename -- path to the .srf file
+    normalize -- normalize the sliprate to 1
+    """
     with open(filename, "rt") as fh:
         return _read_srf(fh, normalize=normalize)
 
