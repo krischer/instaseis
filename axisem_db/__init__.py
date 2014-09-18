18041bf81e8157379c540aceafea9030c013d6ba
martinvandriel <martin@vandriel.de>
2014-09-18 17:36:40 +0200

    adding a basic force source (no stf for now)

--- axisem_db/__init__.py
+++ axisem_db/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import
 
 from .axisem_db import AxiSEMDB  # NoQa
-from .source import Source, Receiver  # NoQa
+from .source import Source, Receiver, ForceSource  # NoQa
