db831da8b0187d816681051d3a800c52abe88f50
martinvandriel <vandriel@erdw.ethz.ch>
2014-10-08 11:32:35 +0200

    rewriting rotations in python. As these are not
    time dependent in the instaseis case, they are not performance critical anyway

--- instaseis/tests/test_rotations.py
+++ instaseis/tests/test_rotations.py
@@ -16,6 +16,31 @@ import numpy as np
 
 from .. import rotations
 
+def test_rotate_tensor_xyz_earth_to_xyz_src():
+    mt = np.array([1., 2., 3., 4., 5., 6.])
+    phi = np.radians(13.)
+    theta = np.radians(29.)
+    mt_rot = rotations.rotate_symm_tensor_voigt_xyz_earth_to_xyz_src(mt, phi, theta)
+    mt_ref = np.array([-1.37383329, -0.68082986, 8.05466315, 5.14580719, 3.34719916, 3.56407819])
+    np.testing.assert_allclose(mt_ref, mt_rot, atol=1e-10)
+
+
+def test_rotate_tensor_xyz_src_to_xyz_earth():
+    mt = np.array([1., 2., 3., 4., 5., 6.])
+    phi = np.radians(13.)
+    theta = np.radians(29.)
+    mt_rot = rotations.rotate_symm_tensor_voigt_xyz_src_to_xyz_earth(mt, phi, theta)
+    mt_ref = np.array([2.37201346, 5.33830776, -1.71032122, 1.36130796, 3.27536413, 7.2728428])
+    np.testing.assert_allclose(mt_ref, mt_rot, atol=1e-10)
+
+
+def test_rotate_tensor_xyz_to_src():
+    mt = np.array([1., 2., 3., 4., 5., 6.])
+    phi = np.radians(13.)
+    mt_rot = rotations.rotate_symm_tensor_voigt_xyz_to_src(mt, phi)
+    mt_ref = np.array([3.68082986, -0.68082986, 3., 2.77272499, 5.77165454, 5.61194985])
+    np.testing.assert_allclose(mt_ref, mt_rot, atol=1e-10)
+
 
 def test_rotate_vector_src_to_xyz():
     # identity
