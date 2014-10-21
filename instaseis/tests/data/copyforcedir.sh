494c26745f0d4101cdb78fc32e923dd9b5d1db19
martinvandriel <vandriel@erdw.ethz.ch>
2014-10-21 21:51:35 +0200

    adding to srcipts to quickly copy all necessary stuff
    from an axisem rundir to the test dir

---
+++ instaseis/tests/data/copyforcedir.sh
@@ -0,0 +1,6 @@
+#/usr/bin/bash
+cp $1/PX/Data/ordered_output.nc4 $2/PX/Data/
+cp $1/PZ/Data/ordered_output.nc4 $2/PZ/Data/
+cp $1/inparam_basic $2/
+cp $1/inparam_advanced $2/
+cp $1/inparam_mesh $2/
---
+++ instaseis/tests/data/copymomentdir.sh
@@ -0,0 +1,8 @@
+#/usr/bin/bash
+cp $1/MXX_P_MYY/Data/ordered_output.nc4 $2/MXX_P_MYY/Data/
+cp $1/MXY_MXX_M_MYY/Data/ordered_output.nc4 $2/MXY_MXX_M_MYY/Data/
+cp $1/MXZ_MYZ/Data/ordered_output.nc4 $2/MXZ_MYZ/Data/
+cp $1/MZZ/Data/ordered_output.nc4 $2/MZZ/Data/
+cp $1/inparam_basic $2/
+cp $1/inparam_advanced $2/
+cp $1/inparam_mesh $2/
