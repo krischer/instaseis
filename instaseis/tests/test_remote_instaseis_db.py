70dc71b1679081a82cea32297044e3e4bb76b872
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-01-04 01:09:21 +0100

    Test for the remote instaseis DB seismogram extraction.

--- instaseis/tests/test_remote_instaseis_db.py
+++ instaseis/tests/test_remote_instaseis_db.py
@@ -43,3 +43,73 @@ def test_info(all_remote_dbs):
         del l_info[key]
 
     assert r_info.__dict__ == l_info.__dict__
+
+
+def _compare_streams(r_db, l_db, kwargs):
+    """
+    Helper function comparing streams extracted from local and remote
+    instaseis databases.
+    """
+    r_st = r_db.get_seismograms(**kwargs)
+    l_st = l_db.get_seismograms(**kwargs)
+
+    assert len(r_st) == len(l_st)
+
+    for r_tr, l_tr in zip(r_st, l_st):
+        assert r_tr.stats.__dict__ == l_tr.stats.__dict__
+        # Very small values have issues with floating point accuracy. 7
+        # orders of magnitude should be more than accurate enough.
+        np.testing.assert_allclose(r_tr.data, l_tr.data,
+                                   atol=1E-7 * r_tr.data.ptp())
+
+
+@responses.activate
+def test_seismogram_extraction(all_remote_dbs):
+    """
+    Test the seismogram extraction from local and remote databases.
+    """
+    # Remote and local database.
+    r_db = all_remote_dbs
+    l_db = instaseis.open_db(r_db._client.filepath)
+    # Mock responses to get the tornado testing to work.
+    _add_callback(r_db._client)
+
+    source = instaseis.Source(
+        latitude=4., longitude=3.0, depth_in_m=0, m_rr=4.71e+17, m_tt=3.81e+17,
+        m_pp=-4.74e+17, m_rt=3.99e+17, m_rp=-8.05e+17, m_tp=-1.23e+17)
+
+    receiver = instaseis.Receiver(latitude=10., longitude=20., depth_in_m=None)
+
+    kwargs = {"source": source, "receiver": receiver,
+              "components": ["Z", "N", "E", "R", "T"]}
+    _compare_streams(r_db, l_db, kwargs)
+
+    # Test velocity and acceleration.
+    kwargs = {"source": source, "receiver": receiver,
+              "components": ["Z", "N", "E", "R", "T"], "kind": "velocity"}
+    _compare_streams(r_db, l_db, kwargs)
+    kwargs = {"source": source, "receiver": receiver,
+              "components": ["Z", "N", "E", "R", "T"], "kind": "acceleration"}
+    _compare_streams(r_db, l_db, kwargs)
+
+    # Test remove source shift.
+    kwargs = {"source": source, "receiver": receiver,
+              "components": ["Z", "N", "E", "R", "T"],
+              "remove_source_shift": False}
+    _compare_streams(r_db, l_db, kwargs)
+
+    # Test lanzcos resampling.
+    kwargs = {"source": source, "receiver": receiver,
+              "components": ["Z", "N", "E", "R", "T"],
+              "dt": 1.0, "a_lanczos": 6}
+    _compare_streams(r_db, l_db, kwargs)
+
+    # Test force source.
+    if "displ_only" in r_db._client.filepath:
+        source = instaseis.ForceSource(
+            latitude=89.91, longitude=0.0, depth_in_m=12000,
+            f_r=1.23E10,
+            f_t=2.55E10,
+            f_p=1.73E10)
+        kwargs = {"source": source, "receiver": receiver}
+        _compare_streams(r_db, l_db, kwargs)
