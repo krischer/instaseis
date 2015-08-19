18aa05f154833bd73d17edff64a8d6b70c512762
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2015-08-19 16:52:55 +0200

    Making the /raw route async

--- instaseis/server/routes/seismograms_raw.py
+++ instaseis/server/routes/seismograms_raw.py
@@ -15,6 +15,52 @@ import tornado.web
 
 from ... import Source, ForceSource, Receiver
 from ..instaseis_request import InstaseisTimeSeriesHandler
+from ..util import run_async
+
+
+@run_async
+def _get_seismogram(db, source, receiver, components, callback):
+    """
+    Extract a seismogram from the passed db and write it either to a MiniSEED
+    or a SACZIP file.
+
+    :param db: An open instaseis database.
+    :param source: An instaseis source.
+    :param receiver: An instaseis receiver.
+    :param components: The components.
+    :param callback: callback function of the coroutine.
+    """
+    # Get the most barebones seismograms possible.
+    try:
+        db._get_seismograms_sanity_checks(
+            source=source, receiver=receiver, components=components,
+            kind="displacement")
+        data = db._get_seismograms(
+            source=source, receiver=receiver, components=components)
+    except Exception:
+        msg = ("Could not extract seismogram. Make sure, the components "
+               "are valid, and the depth settings are correct.")
+        callback(tornado.web.HTTPError(400, log_message=msg, reason=msg))
+        return
+
+    try:
+        st = db._convert_to_stream(
+            receiver=receiver, components=components,
+            data=data, dt_out=db.info.dt, starttime=source.origin_time)
+    except Exception:
+        msg = ("Could not convert seismogram to a Stream object.")
+        callback(tornado.web.HTTPError(500, log_message=msg, reason=msg))
+        return
+
+    # Half the filesize but definitely sufficiently accurate.
+    for tr in st:
+        tr.data = np.require(tr.data, dtype=np.float32)
+
+    with io.BytesIO() as fh:
+        st.write(fh, format="mseed")
+        fh.seek(0, 0)
+        binary_data = fh.read()
+    callback((binary_data, st[0].stats.instaseis.mu))
 
 
 class RawSeismogramsHandler(InstaseisTimeSeriesHandler):
@@ -56,6 +102,8 @@ class RawSeismogramsHandler(InstaseisTimeSeriesHandler):
     def validate_parameters(self, args):
         pass
 
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
     def get(self):
         args = self.parse_arguments()
 
@@ -135,39 +183,18 @@ class RawSeismogramsHandler(InstaseisTimeSeriesHandler):
                    "Check parameters for sanity.")
             raise tornado.web.HTTPError(400, log_message=msg, reason=msg)
 
-        # Get the most barebones seismograms possible.
-        try:
-            self.application.db._get_seismograms_sanity_checks(
-                source=source, receiver=receiver, components=components,
-                kind="displacement")
-            data = self.application.db._get_seismograms(
-                source=source, receiver=receiver, components=components)
-        except Exception:
-            msg = ("Could not extract seismogram. Make sure, the components "
-                   "are valid, and the depth settings are correct.")
-            raise tornado.web.HTTPError(400, log_message=msg, reason=msg)
+        response = yield tornado.gen.Task(
+            _get_seismogram, db=self.application.db, source=source,
+            receiver=receiver, components=components)
 
-        try:
-            st = self.application.db._convert_to_stream(
-                receiver=receiver, components=components,
-                data=data, dt_out=self.application.db.info.dt,
-                starttime=args.origintime)
-        except Exception:
-            msg = ("Could not convert seismogram to a Stream object.")
-            raise tornado.web.HTTPError(500, log_message=msg, reason=msg)
-
-        # Half the filesize but definitely sufficiently accurate.
-        for tr in st:
-            tr.data = np.require(tr.data, dtype=np.float32)
-
-        with io.BytesIO() as fh:
-            st.write(fh, format="mseed")
-            fh.seek(0, 0)
-            binary_data = fh.read()
+        # If an exception is returned from the task, re-raise it here.
+        if isinstance(response, Exception):
+            raise response
 
         self.set_headers(args)
         # Passing mu in the HTTP header...not sure how well this plays with
         # proxies...
-        self.set_header("Instaseis-Mu", "%f" % st[0].stats.instaseis.mu)
+        self.set_header("Instaseis-Mu", "%f" % response[1])
 
-        self.write(binary_data)
+        self.write(response[0])
+        self.finish()
