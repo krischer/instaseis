90c0bd4de25153f23f2d1a5b819c9fa187790e9d
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-01-15 14:49:52 +0100

    Repacking to a single merged file for two components.

--- instaseis/scripts/repack_instaseis_database.py
+++ instaseis/scripts/repack_instaseis_database.py
@@ -143,12 +143,112 @@ def process_file(input_filename, output_filename, method):
         except:
             pass
 
+def unroll_and_merge(filenames, output_folder):
+    """
+    Completely unroll and merge both files.
+    """
+    # Find PX and PZ files.
+    assert len(filenames) == 2
+    filenames = [os.path.normpath(_i) for _i in filenames]
+    px = [_i for _i in filenames if "PX" in _i]
+    pz = [_i for _i in filenames if "PX" in _i]
+    assert len(px) == 1
+    assert len(pz) == 1
+    px = px[0]
+    pz = pz[0]
+
+    output_filename = os.path.join(output_folder, "merged_instaseis_db.nc4")
+    assert not os.path.exists(output_filename)
+
+    assert os.path.exists(px)
+    assert os.path.exists(pz)
+    assert not os.path.exists(output_filename)
+
+    try:
+        f_in_x = h5py.File(px, "r")
+        f_in_z = h5py.File(pz, "r")
+        f_out = h5py.File(output_filename, libver="latest")
+
+        # Copy attributes from the vertical file.
+        for key, value in f_in_z.attrs.items():
+            f_out.attrs[key] = value
+
+        # Same from simple groups.
+        for group in f_in_z.keys():
+            # Special cased later on.
+            if group == "Snapshots":
+                continue
+            click.echo(click.style("\tCopying group '%s'..." % group,
+                                   fg="blue"))
+            f_out.copy(f_in_z[group], group)
+
+        # Create a new array but this time in 5D. The first dimension
+        # is the element number, the second and third are the GLL
+        # points in both directions, the fourth is the time axis, and the
+        # last the displacement axis.
+        npts = f_in_z.attrs["number of strain dumps"][0]
+        number_of_elements = f_in_z.attrs["nelem_kwf_global"][0]
+        npol = f_in_z.attrs["npol"][0]
+
+        # Get datasets and the dtype.
+        meshes = [
+            f_in_x["Snapshots"]["disp_s"],
+            f_in_x["Snapshots"]["disp_p"],
+            f_in_x["Snapshots"]["disp_z"],
+            f_in_z["Snapshots"]["disp_s"],
+            f_in_z["Snapshots"]["disp_z"]]
+        dtype = meshes[0].dtype
+
+        ds_o = f_out.create_dataset(
+            "unrolled_snapshots",
+            shape=(number_of_elements, npts, npol + 1, npol + 1, 5),
+            dtype=dtype, chunks=None, compression=None)
+
+        utemp = np.zeros((npts, npol + 1, npol + 1, 5), dtype=dtype, order="F")
+
+        # Now it becomes more interesting and very slow.
+        sem_mesh = f_in_x["Mesh"]["sem_mesh"]
+        with click.progressbar(range(number_of_elements),
+                               length=number_of_elements,
+                               label="\t  ") as idx:
+            for gll_idx in idx:
+                gll_point_ids = sem_mesh[gll_idx]
+
+                # Load displacement from all GLL points.
+                for i, var in enumerate(meshes):
+                    # The list of ids we have is unique but not sorted.
+                    ids = gll_point_ids.flatten()
+                    s_ids = np.sort(ids)
+                    temp = var[:, s_ids]
+                    for ipol in range(npol + 1):
+                        for jpol in range(npol + 1):
+                            idx = ipol * 5 + jpol
+                            utemp[:, jpol, ipol, i] = \
+                                temp[:,
+                                np.argwhere(s_ids == ids[idx])[0][0]]
+                ds_o[gll_idx] = utemp
+
+    finally:
+        try:
+            f_in_x.close()
+        except:
+            pass
+        try:
+            f_in_z.close()
+        except:
+            pass
+        try:
+            f_out.close()
+        except:
+            pass
+
 
 @click.command()
 @click.argument("input_folder", type=click.Path(exists=True, file_okay=False,
                                                 dir_okay=True))
 @click.argument("output_folder", type=click.Path(exists=False))
-@click.option('--method', type=click.Choice(["continuous", "unrolled"]),
+@click.option('--method', type=click.Choice(["continuous", "unrolled",
+                                             "unrolled_merge"]),
               help="'continuous' will transpose all arrays and store them "
               "unchunked and uncompressed. 'unrolled' will completely unroll "
               "all arrays into one. It is a slow transformation and also "
@@ -165,18 +265,24 @@ def repack_database(input_folder, output_folder, method):
 
     os.makedirs(output_folder)
 
-    for _i, filename in enumerate(found_filenames):
-        click.echo(click.style(
-            "--> Processing file %i of %i: %s" %
-            (_i + 1, len(found_filenames), filename), fg="green"))
+    # The unrolled merge completely unrolls everything, dededuplicates the GLL
+    # points, and merges both netCDF files into one big file.
+    if method == "unrolled_merge":
+        unroll_and_merge(filenames=found_filenames,
+                         output_folder=output_folder)
+    else:
+        for _i, filename in enumerate(found_filenames):
+            click.echo(click.style(
+                "--> Processing file %i of %i: %s" %
+                (_i + 1, len(found_filenames), filename), fg="green"))
 
-        output_filename = os.path.join(
-            output_folder,
-            os.path.relpath(filename, input_folder))
+            output_filename = os.path.join(
+                output_folder,
+                os.path.relpath(filename, input_folder))
 
-        os.makedirs(os.path.dirname(output_filename))
+            os.makedirs(os.path.dirname(output_filename))
 
-        process_file(filename, output_filename, method=method)
+            process_file(filename, output_filename, method=method)
 
 
 if __name__ == "__main__":
