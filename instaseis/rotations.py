Fixing small numerical issue that only affects the tests

instaseis/rotations.py
@@ -66,8 +66,18 @@ def rotate_symm_tensor_voigt_xyz_earth_to_xyz_src(mt, phi, theta):
                   [ct * sp, cp, st * sp],
                   [-st, 0, ct]])
 
+    # This double matrix product involves number that might differ by 20
+    # orders of magnitudes which makes it numerically tricky. Thus we employ
+    # quad precision numbers to make it a bit more stable and reproducable.
+    R = np.require(R, dtype=np.float128)
+    A = np.require(A, dtype=np.float128)
+
     B = np.dot(np.dot(R.T, A), R)
-    return np.array([B[0, 0], B[1, 1], B[2, 2], B[1, 2], B[0, 2], B[0, 1]])
+
+    # Convert back to single precision.
+    return np.require(
+        np.array([B[0, 0], B[1, 1], B[2, 2], B[1, 2], B[0, 2], B[0, 1]]),
+        dtype=np.float64)
 
 
 def rotate_symm_tensor_voigt_xyz_src_to_xyz_earth(mt, phi, theta):

instaseis/tests/test_source.py
@@ -302,10 +302,9 @@ def test_CMT_finite_source():
     finitesource.compute_centroid()
 
     np.testing.assert_allclose(
-        # XXX: Why did this change???j
-        np.array([-3.91886976e+04, 3.90905071e+04, 9.81905182e+01,
-                  1.94225428e+04, -5.267504e+03, 3.19598660e+20]),
-        finitesource.CMT.tensor_voigt)
+        np.array([-3.918870e+04, 3.909051e+04, 9.819052e+01,
+                  1.942254e+04, -5.476000e+03, 3.195987e+20]),
+        finitesource.CMT.tensor_voigt, rtol=1E-5)
 
     np.testing.assert_allclose(np.array([finitesource.CMT.latitude]),
                                np.array([0.0]))