0178a56666f7347cbe5afe878d28d9ace94e4152
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-02-24 11:12:33 +0100

    Fine tuning the fix

--- instaseis/source.py
+++ instaseis/source.py
@@ -900,11 +900,13 @@ class Receiver(SourceOrReceiver):
             if not hasattr(filename_or_obj.stats, "sac"):
                 raise ReceiverParseError("ObsPy Trace must have an sac "
                                          "attribute.")
-            if not "stla" not in filename_or_obj.stats.sac or \
+            if "stla" not in filename_or_obj.stats.sac or \
                     "stlo" not in filename_or_obj.stats.sac:
                 raise ReceiverParseError(
                     "SAC file does not contain coordinates for channel '%s'" %
                     filename_or_obj.id)
+            coords = (filename_or_obj.stats.sac.stla,
+                      filename_or_obj.stats.sac.stlo)
             return [Receiver(
                 latitude=elliptic_to_geocentric_latitude(coords[0]),
                 longitude=coords[1],
