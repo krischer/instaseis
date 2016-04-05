b32da76bfe4a7a0c73b1ef282cf40c96327f7042
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-04-05 14:43:08 +0200

    Add dedicated buffer test

---
+++ instaseis/tests/test_buffer.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Tests for the array buffer.
+
+:copyright:
+    Lion Krischer (krischer@geophysik.uni-muenchen.de), 2016
+:license:
+    GNU Lesser General Public License, Version 3 [non-commercial/academic use]
+    (http://www.gnu.org/copyleft/lgpl.html)
+"""
+from __future__ import absolute_import
+
+import numpy as np
+
+from instaseis.mesh import Buffer
+
+
+def test_buffer():
+    buf = Buffer(max_size_in_mb=1.0)
+    assert buf.efficiency == 0.0
+    # Exactly one bytes less then a megabyte.
+    buf.add("a", np.empty(1024 ** 2 - 1, dtype=np.int8))
+
+    assert "a" in buf
+    assert buf._total_size == 1024 ** 2 - 1
+
+    buf.add("b", np.empty(2, dtype=np.int8))
+
+    assert "a" not in buf
+    assert "b" in buf
+    assert buf._total_size == 2
+
+    buf.add("c", np.empty(2, dtype=np.int8))
+    assert buf._total_size == 4
+
+    assert buf.get_size_mb() == 4 / (1024 ** 2)
+
+    # Twice in, once not.
+    assert buf.efficiency == 2.0 / 3.0
+
+    # Once more not in.
+    assert "d" not in buf
+    assert buf.efficiency == 2.0 / 4.0
