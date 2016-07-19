f987ad75e78e1c07e5f2174a10716d64eb3c1e45
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-07-19 22:09:33 +0200

    Searching the /Snapshots group first for the STF arrays.

--- instaseis/database_interfaces/mesh.py
+++ instaseis/database_interfaces/mesh.py
@@ -194,7 +194,9 @@ class Mesh(object):
         self.source_shift_samp = self.f.attrs[
             "source shift factor for deltat_coarse"][0]
 
-        possible_stf_groups = ["Surface", "Snapshots", "/"]
+        # Search /Snapshots first - then the root group and then the legacy
+        # /Surface group.
+        possible_stf_groups = ["Snapshots", "/", "Surface"]
         found_stf = False
         for g in possible_stf_groups:
             if g not in self.f:  # pragma: no cover
