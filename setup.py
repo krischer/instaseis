292f0b7d532fbedc0f7581ce427cf9c542d60517
Lion Krischer <lion.krischer@gmail.com>
2017-04-25 15:04:13 +0200

    Adding Python 3.6 trove.

--- setup.py
+++ setup.py
@@ -194,6 +194,7 @@ setup_config = dict(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Physics"
