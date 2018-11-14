a23c08c99f30d8ed44f9b7ddf35b02be1e0c72d9
Travis <travis@travis-ci.org>
2018-11-14 01:37:24 +0000

    Travis build 497 pushed to gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.3.0-6-g9b97',
+    VERSION: '1.3.0-8-gb93a499',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
